1835df3361b734bc6daf6d972020689ecad8c3cd
gquesnot <g.quesnot@marquedigitale.fr>
2020-11-09 14:18:11 +0100

    start

---
+++ script/controleur.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(function(){
+	var catalog = [{
+		"name":"test",
+		"image": "images/test.jpg",
+		"price": "50"
+	},
+	{
+		"name":"test2",
+		"image":"images/test2.jpg",
+		"price": "40"
+	}];
+});
