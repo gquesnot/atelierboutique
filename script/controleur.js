79ab1bcfd183fa0b4b327d9dea03b2a7d504289c
gquesnot <g.quesnot@marquedigitale.fr>
2020-11-09 15:12:03 +0100

    change src images add js

---
+++ script/controleur.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$(function(){
+	
+
+	var baseCatalog = catalog;
+
+	
+
+
+	/*$('#search').change(function(){
+		var search = this.val()
+		if (this.value() != ""){
+			$('#catalog li').each(function(article){
+				if (article.name.includes(this.value()))
+					
+
+			});
+		}
+		
+	});*/
+
+	function setCatalog(catalogId){
+		import 
+	}
+
+
+});
