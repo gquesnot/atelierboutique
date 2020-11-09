c8c0a0350704a34ac662ebbff42671fbd4ee32d3
gquesnot <g.quesnot@marquedigitale.fr>
2020-11-09 17:36:34 +0100

    comment

--- script/generatearticle.js
+++ script/generatearticle.js
@@ -1,7 +1,9 @@
 import {catalog} from "../data/catalogue.js";
 
+//genere ou regenere les articles dans la div #catalogue
 export function generateArticle(id)
 		{
+			//select catalog
 			if (id == 1)
 				var actualCatalogue = catalog;
 			else if (id == 2)
@@ -9,10 +11,16 @@ export function generateArticle(id)
 			else
 				return;
 			var res= "";
+			//concatene "" avec chaque article le \ permet les sauts de ligne mais en  realité c'est une string
 			actualCatalogue.forEach(function(article){
-				res += "<div class=\"article\"><h3>"+article.name+"</h3><img src=\""+ article.image +"\"/></div>";
+				res += "<div class=\"article\">\
+							<h3>"+article.name+"</h3>\
+							<img src=\""+ article.image +"\"/>\
+						</div>";
 				
 			});
-			$('#catalogue').innerHTML = "";
+			//vide #catalogue 
+			$('#catalogue').empty();
+			//rempli #catalogue  avec les nouveaux articles
 			$('#catalogue').append(res);
 		}
--- script/search.js
+++ script/search.js
@@ -1,17 +1,19 @@
+//affiche ou cache les articles en fonction de l'input #search
 export function search()
 	{
+		// == pour chaque changement dans #search /!\ ici "input" veut dire :si l'utilisateur ecris qqc dans #search
 		$('#search').on("input",function(){
-			var toSearch = this.value;
-			if (toSearch != ""){
-				$('#catalogue div').each(function(article){
-					var name = $(this).find("h3")[0].textContent;
-					if (name.toLowerCase().includes(toSearch.toLowerCase()))
-						$(this).show();
-					else
-						$(this).hide();
-				});
-					
-			}
-			
+			//valeur de #search
+			var toSearch = $(this).val();
+			// pour chaque div dans #catalogue
+			$('#catalogue div').each(function(article){
+				// name = text dans le h3 de l'article
+				var name = $(this).find("h3")[0].textContent;
+				// La méthode includes() détermine si une chaîne de caractères est contenue dans une autre
+				if (name.toLowerCase().includes(toSearch.toLowerCase()))
+					$(this).show();
+				else
+					$(this).hide();
+			});
 		});
 	}
